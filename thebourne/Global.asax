8b43128b1b2732fd1867d20263863767da4e28b5
Mark Christopher Cacal <markchristopher.cacal@outlook.com>
2019-03-04 15:04:31 +0800

    Add project files.

---
+++ thebourne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="thebourne.MvcApplication" Language="C#" %>
---
+++ thebourne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(thebourne.Startup))]
+namespace thebourne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
